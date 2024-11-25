01176febc9a98c17e7357830e6ee397225606772
txaty <txaty@proton.me>
2024-11-25 17:42:19 +0800

    chore: Codebase refactor

--- halo2_proofs/examples/multi-row-plonk.rs
+++ halo2_proofs/examples/multi-row-plonk.rs
@@ -241,7 +241,7 @@ impl<F: Field> Circuit<F> for AddCircuit64<F> {
             let a: Value<Assigned<_>> = self.a.into();
             let b: Value<Assigned<_>> = self.b.into();
             let mut a_add_b = Value::unknown();
-            let (_, _, c) = cs.add(&mut layouter, || {
+            let _ = cs.add(&mut layouter, || {
                 a_add_b = a + b;
                 a.zip(b)
                     .zip(a_add_b)
@@ -284,11 +284,11 @@ impl<F: Field> Circuit<F> for MulCircuit64<F> {
     ) -> Result<(), ErrorFront> {
         let cs = Plonk::new(config);
 
-        for i in 0..SEGMENT_SIZE {
+        for _ in 0..SEGMENT_SIZE {
             let a: Value<Assigned<_>> = self.a.into();
             let b: Value<Assigned<_>> = self.b.into();
             let mut a_mul_b = Value::unknown();
-            let (_, _, c) = cs.multiply(&mut layouter, || {
+            let _ = cs.multiply(&mut layouter, || {
                 a_mul_b = a * b;
                 a.zip(b)
                     .zip(a_mul_b)
@@ -360,8 +360,8 @@ impl<F: Field> Circuit<F> for WitnessCircuit64<F> {
             let a: Value<Assigned<_>> = self.left_values[i].into();
             let b: Value<Assigned<_>> = self.right_values[i].into();
             let mut res = Value::unknown();
-            for j in 0..SEGMENT_SIZE {
-                let (_, _, c) = match self.queried_circuit_indices[i] {
+            for _ in 0..SEGMENT_SIZE {
+                let _ = match self.queried_circuit_indices[i] {
                     0 => cs.add(&mut layouter, || {
                         res = a + b;
                         a.zip(b).zip(res).map(|((a, b), res)| (a, b, res))
